5ff9894c90fe76fba094bb3aebeb5a2409eaf313
shedboy71 <webmaster@ihservices.co.uk>
2023-07-02 16:44:17 +0100

    Add files via upload

---
+++ Examples/arduinolcdandccs811/arduinolcdandccs811.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#include "Adafruit_CCS811.h"
+
+//setup for the LCD keypad shield
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+Adafruit_CCS811 ccs;
+
+void setup() 
+{
+  //init serial for some debug
+  Serial.begin(9600);
+  Serial.println("CCS811 test"); 
+  //init CCS811 sensor
+  if(!ccs.begin())
+  {
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
+  }
+  //calibrate temperature sensor
+  while(!ccs.available());
+  float temp = ccs.calculateTemperature();
+  ccs.setTempOffset(temp - 25.0);
+  //init LCD
+  lcd.begin(16,2);
+  //line 1 - temperature
+  lcd.setCursor(0,0);
+  lcd.print("Temp = ");
+  //line 2 - humidity
+  lcd.setCursor(0,1);
+  lcd.print("Hum = ");
+}
+
+void loop() 
+{
+  //display temperature on lcd
+  if(ccs.available())
+  {
+  float temp = ccs.calculateTemperature();
+    if(!ccs.readData())
+    {
+      lcd.setCursor(0,0);
+      lcd.print("CO2: ");
+      lcd.print(ccs.geteCO2());
+      lcd.print(" ppm");
+      lcd.setCursor(0,1);
+      lcd.print("TVOC: ");
+      lcd.print(ccs.getTVOC());
+  
+    }
+    else
+    {
+      Serial.println("ERROR!");
+      while(1);
+    }
+  }
+  delay(500);
+}
+
+
---
+++ Examples/arduinolcdandmlx90615/arduinolcdandmlx90615.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#include <mlx90615.h>
+
+//setup for the LCD keypad shield
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+MLX90615 mlx = MLX90615();
+
+void setup() 
+{
+  //init serial for some debug
+  Serial.begin(9600);
+  Serial.println("Melexis MLX90615 infra-red temperature sensor test");
+  //init MLX90615 sensor
+  mlx.begin();
+  Serial.print("Sensor ID number = ");
+  Serial.println(mlx.get_id(), HEX);
+  //init LCD
+  lcd.begin(16,2);
+  //line 1 - temperature
+  lcd.setCursor(0,0);
+  lcd.print("AMB = ");
+  //line 2 - humidity
+  lcd.setCursor(0,1);
+  lcd.print("OBJ = ");
+}
+
+void loop() 
+{
+  //display temperature on lcd
+  
+  lcd.setCursor(0,0);
+  lcd.print("AMB = ");
+  lcd.print(mlx.get_ambient_temp());
+  lcd.print(" *c");
+  lcd.setCursor(0,1);
+  lcd.print("OBJ = ");
+  lcd.print(mlx.get_object_temp());
+  lcd.print(" *c");
+
+  delay(500);
+}
+
+
